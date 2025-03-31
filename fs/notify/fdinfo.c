fe5f9fe9799313f2d94763326a17bb96bea4407e
texascake soft <tiktodz@soulvibe-team.cloud>
2025-03-31 14:37:43 +0000

    Revert "inotify: hide internal kernel bits from fdinfo"
    This reverts commit 6933599697c96c3213c95f5f1fc7cb6abfd08c54.
    
    Change-Id: I09e73715d93bccb9a14c3822ce9c711a6d534c97

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -132,16 +132,10 @@ out_free_pathname:
 		}
 out_seq_printf:
 #endif
-		/*
-		 * IN_ALL_EVENTS represents all of the mask bits
-		 * that we expose to userspace.  There is at
-		 * least one bit (FS_EVENT_ON_CHILD) which is
-		 * used only internally to the kernel.
-		 */
-		//u32 mask = mark->mask & IN_ALL_EVENTS;
+
 		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:%x ",
 			   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
-			   mask, mark->ignored_mask);
+			   mark->mask, mark->ignored_mask);
 		show_mark_fhandle(m, inode);
 		seq_putc(m, '\n');
 		iput(inode);
