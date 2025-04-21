b10b5930cc1bd6ab4bc969b2698e1ba59c7f7ffd
Kneba <abenkenary3@gmail.com>
2025-04-21 16:42:56 +0000

    Revert "drivers: power: Disable thermal limit"
    This reverts commit e5d138c78ff4929dcbd5d19c55be574b3bb9ac67.

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1013,7 +1013,7 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		rc = smblib_get_prop_batt_capacity(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
-		rc = 0;
+		rc = smblib_get_prop_system_temp_level(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_CHARGER_TEMP:
 		/* do not query RRADC if charger is not present */
@@ -1135,7 +1135,7 @@ static int smb2_batt_set_prop(struct power_supply *psy,
 		break;
 #endif
 	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
-		rc = 0;
+		rc = smblib_set_prop_system_temp_level(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
 		rc = smblib_set_prop_batt_capacity(chg, val);
