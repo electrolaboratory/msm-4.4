8b0458df059c9659e56e8ae28055d588e350d9e6
Kneba <abenkenary3@gmail.com>
2025-04-22 17:03:31 +0000

    cmdline: back to hack to bypass charging boot
    Change-Id: I33e7a54af4df9b73157dc1fba97ffe196dc84248

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,13 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_puts(m, saved_command_line);
+	seq_puts(m, new_command_line);
 	seq_putc(m, '\n');
 	return 0;
 }
@@ -24,6 +27,35 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+	char *offset_addr, *cmd = new_command_line;
+
+	strcpy(cmd, saved_command_line);
+
+	if (offset_addr) {
+ 		size_t i, len, offset;
+ 
+ 		len = strlen(cmd);
+ 		offset = offset_addr - cmd;
+ 
+ 		for (i = 1; i < (len - offset); i++) {
+ 			if (cmd[offset + i] == ' ')
+ 				break;
+ 		}
+ 
+ 		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+ 	}
+
+	offset_addr = strstr(cmd, "startup=");
+ 	if (offset_addr) {
+ 		const char *magic = "0x00000001"; // BIT 1 = POWER KEY | BIT 3 = CHARGING
+ 		char *s = &offset_addr[8]; // locate 0x00000000
+ 		size_t i;
+ 
+ 		for (i = 0; i < strlen(magic); i++) {
+ 			s[i] = magic[i];
+ 		}
+ 	}
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
